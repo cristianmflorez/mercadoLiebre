61be8f62112bb47384efa0f7f69ff1c7b1c9b023
cristianmflorez <cristianmflorez@gmail.com>
2022-09-07 11:55:04 -0500

    version final 3 vistas

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get('/', (req, res)=>{
     res.sendFile((__dirname + '/views/home.html'));
 });
 
+app.get('/login', (req, res)=>{
+    res.sendFile((__dirname + '/views/login.html'));
+});
+
+app.get('/crear', (req, res)=>{
+    res.sendFile((__dirname + '/views/crearCuenta.html'));
+});
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
